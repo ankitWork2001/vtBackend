9aaa98a2c7403d1abb407c2cfacb6150dc6339ad
Rohit Sharma <rsantoshsharma9730@gmail.com>
2025-06-06 18:14:12 +0530

    My testimonial work

--- routes/testimonialRoutes.js
+++ routes/testimonialRoutes.js
@@ -1,6 +1,11 @@
 import express from 'express';
 const router = express.Router();
+import { TestimonialRouter } from "../controllers/testimonialController";
 
-router.get('/test');
+
+router.post('/addtestimonial', TestimonialRouter.addTestimonial);
+router.get('/getAllApprovedTestimonials', TestimonialRouter.getAllApprovedTestimonials);
+router.get('/getAllTestimonials', TestimonialRouter.getAllTestimonials);
+router.gst('/approvedTestimonial', TestimonialRouter.approvedTestimonial);
 
 export { router as testimonialRouter };

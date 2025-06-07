6aeb3b49c47e2dc135dd61491a1cdb8e7179f112
Rohit Sharma <rsantoshsharma9730@gmail.com>
2025-06-07 18:59:32 +0530

    Testimonial work done

--- controllers/messageController.js
+++
@@ -1,3 +0,0 @@
-export const messageController = (req, res) => {
-
-};
--- routes/testimonialRoutes.js
+++ routes/testimonialRoutes.js
@@ -4,5 +4,8 @@ import * as TestimonialRouter from "../controllers/testimonialController.js";
 
 
 router.post('/addtestimonial', TestimonialRouter.addTestimonial);
+router.get('/approved', TestimonialRouter.getAllApprovedTestimonials);
+router.get('/', TestimonialRouter.getAllTestimonials);
+router.put('/id/approve', TestimonialRouter.approvedTestimonial);
 
 export { router as testimonialRouter };

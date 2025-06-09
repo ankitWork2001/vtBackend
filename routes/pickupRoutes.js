071310be6689d03611b79a8d1952d8d32f25144b
Akashsahani47 <ak676964@gmail.com>
2025-06-09 16:01:10 +0530

    succesullyDoneLogin-Sign

--- routes/pickupRoutes.js
+++ routes/pickupRoutes.js
@@ -1,11 +1,7 @@
 import express from 'express';
-import { verifyToken } from '../middleware/authMiddleware';
 const router = express.Router();
 
-
-
-
-router.post("/pickup-information",verifyToken, createPickupInfo);
+router.post("/pickup-information", createPickupInfo);
 
 
 

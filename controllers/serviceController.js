64e9e7ec6321eb1c0b456d080784c879d5f2eef5
swapnasruthi <swapnasruthi2005@gmail.com>
2025-06-09 16:32:57 +0530

    removed service & btestimonial

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -1,32 +1,3 @@
-import {ServiceModel} from "../models/Service.js";
+export const servicesController = (req, res) => {
 
-
-//to get all the services
-export const getAllServices = async (req, res) => {
-  try {
-    const services = await ServiceModel.find();
-    res.status(200).json(services);
-  } catch (err) {
-    res.status(500).json({ message: 'Server error' });
-  }
-};
-
-
-
-//to fetch user services
-
-export const getUserServices = async (req, res) => {
-  try {
-    const userId = req.user?._id;
-
-    if (!userId) {
-      return res.status(401).json({ message: 'Unauthorized: User not logged in' });
-    }
-
-    const services = await ServiceModel.find({ userId });
-    res.status(200).json(services);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Server error' });
-  }
 };
--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -2,7 +2,6 @@ import express from 'express';
 const router = express.Router();
 import * as servicesController from "../controllers/serviceController.js";
 
-router.get('/allservices', servicesController.getAllServices);
-router.get('/userservices', servicesController.getUserServices);
+router.get('/services', servicesController.servicesController);
 
 export { router as servicesRouter };

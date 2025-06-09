870bb956501826c8c65cff8242a47a20f94501b8
Rohit Sharma <rsantoshsharma9730@gmail.com>
2025-06-09 12:55:59 +0530

    Initial project setup with routes and structure

--- routes/testimonialRoutes.js
+++ routes/testimonialRoutes.js
@@ -4,8 +4,5 @@ import * as TestimonialRouter from "../controllers/testimonialController.js";
 
 
 router.post('/addtestimonial', TestimonialRouter.addTestimonial);
-router.get('/approved', TestimonialRouter.getAllApprovedTestimonials);
-router.get('/', TestimonialRouter.getAllTestimonials);
-router.put('/id/approve', TestimonialRouter.approvedTestimonial);
 
 export { router as testimonialRouter };

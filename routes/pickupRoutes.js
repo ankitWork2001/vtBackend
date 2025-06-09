eca60f3d8c1c7c24c109c8d188bc75bfa72baf51
Akashsahani47 <ak676964@gmail.com>
2025-06-09 15:58:56 +0530

    pichupDone

--- routes/pickupRoutes.js
+++ routes/pickupRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import { verifyToken } from '../middleware/authMiddleware.js';
+import { createPickupInfo } from '../controllers/pickupController.js';
 const router = express.Router();
-import * as pickupController from "../controllers/pickupController.js";
 
-router.get('/pickup', pickupController.pickupController);
+router.post("/pickup-information",verifyToken, createPickupInfo);
 
 export { router as pickupRouter };
+

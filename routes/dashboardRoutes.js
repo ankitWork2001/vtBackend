887b1544b04eba19978906030bd6de30fba2724a
swapnasruthi <swapnasruthi2005@gmail.com>
2025-06-13 09:51:38 +0530

    done userDashBoard

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 const router = express.Router();
 import * as dashboardController from "../controllers/dashboardController.js";
+import { verifyToken } from '../middleware/authMiddleware.js';
 
-router.get('/dashboard', dashboardController.dashboardController);
+
+router.get('/dashboard',verifyToken, dashboardController.getDashboardData);
 
 export { router as dashboardRouter };

b0feb048aa7319ed91167236cc11c7c443e6457c
Shivam Mandal <sanchayetamandal83404@gmail.com>
2025-06-13 20:34:05 +0530

    created todo routes and controller

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,3 +1,77 @@
-export const todoController = (req, res) => {
+import { TodoTaskModel } from "../models/ToDoTask.js";
+export const getAllTodoController = async (req, res) => {
+    try {
+        const todo = await TodoTaskModel.find();
+        if (!todo) {
+            return res.status(400).json({ success: false, message: "Items not found!" })
+        }
+        if(todo.length===0){
+            return res.status(400).json({ success: true, message: "Todo is empty.",data:todo })
 
+        }
+        res.status(200).json({ success: true, message: "Successfully found all Items.", data: todo });
+    } catch (err) {
+        res.status(500).json({ success: false, error: err.message })
+    }
+};
+
+export const createTodoController = async (req, res) => {
+    try {
+        const { description, dueDate, isStarred } = req.body;
+        // const userId = req.user.id;
+
+        const userId = req.body.id;
+
+        if (!description || !dueDate || !isStarred) {
+            return res.status(400).json({ success: false, message: "Each field is required." });
+        }
+
+        const newTask = new TodoTaskModel({
+            userId,
+            description,
+            dueDate,
+            isStarred
+        });
+
+        const savedTask = await newTask.save();
+
+        res.status(201).json({ success: true, message: "Todo created successfully.", data: savedTask });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Failed to create todo.", error: error.message });
+    }
+};
+
+export const updateTodoController = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updatedData = req.body;
+        const updateTodo = await TodoTaskModel.findByIdAndUpdate(
+            id,
+            updatedData,
+            { new: true, runValidators: true }
+        );
+
+        if (!updateTodo) {
+            return res.status(400).json({ success: false, message: "Todo not found with this ID" })
+        }
+
+        res.status(200).json({ success: true, message: "Todo updated successfully", data: updateTodo })
+
+
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message })
+    }
+};
+
+export const deleteSpecificTodoController = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteTodo = await TodoTaskModel.findByIdAndDelete(id);
+        if (!deleteTodo) {
+            return res.status(400).json({ success: false, message: "Todo not found with this ID" });
+        }
+        res.status(200).json({ success: true, message: "Todo deleted successfully", data: deleteTodo })
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message })
+    }
 };
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,7 +1,11 @@
 import express from 'express';
 const router = express.Router();
 import * as todoController from "../controllers/todoController.js";
-
-router.get('/todo', todoController.todoController);
+import { verifyToken } from '../middleware/authMiddleware.js'
+import { authorizeRoles } from '../middleware/roleMiddleware.js'
+router.get('/tasks',todoController.getAllTodoController);
+router.post('/tasks',todoController.createTodoController);
+router.put('/tasks/:id',todoController.updateTodoController);
+router.delete('/tasks/:id',todoController.deleteSpecificTodoController);
 
 export { router as todoRouter };

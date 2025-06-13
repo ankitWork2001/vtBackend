7cb314b79270e2f0d8a17eaf4f1c430cef0fd7de
Rohit Kumar <rohitku101223@gmail.com>
2025-06-13 10:04:29 +0530

    update route file

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import {verifyToken} from '../middleware/authMiddleware.js';
 const router = express.Router();
 import * as userController from "../controllers/userController.js";
 
 // router.get('/user', userController.userController);
-router.get('/user/profile', userController.getUserProfile);
-router.put('/user/profileupdate', userController.updateUserProfile);
-router.put('/user/profileAcc', userController.updateUserAccount);
+router.get('/user/profile',verifyToken, userController.getUserProfile);
+router.put('/user/profileupdate',verifyToken, userController.updateUserProfile);
+router.put('/user/profileAcc',verifyToken, userController.updateUserAccount);
 
 export { router as userRouter };

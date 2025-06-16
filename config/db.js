066ad85fdead8876114781debe8b1e341ebb3d15
Shivam Mandal <sanchayetamandal83404@gmail.com>
2025-06-16 12:20:46 +0530

    created order controller and routes

--- config/db.js
+++ config/db.js
@@ -2,13 +2,14 @@ import dotenv from 'dotenv'
 dotenv.config()
 import mongoose from "mongoose";
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
+  mongoose.connect(process.env.MONGO_URI , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => {
     console.log("MongoDB connected");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-    process.exit(1);
-  }
+  }).catch(err => {
+    console.error("MongoDB connection error:", err);
+  });
 };
 
 export default connectDB;
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,6 +2,12 @@ import express from 'express';
 const router = express.Router();
 import * as orderController from "../controllers/orderController.js";
 
-router.get('/order', orderController.orderController);
+// router.post('/createorder/:id', orderController.createOrder);
+router.get('/allorder', orderController.getAllOrders);
+router.get('/specificorder/:id', orderController.getSpecificOrder);
+router.put('/getspecific/:id/status', orderController.updateOrderStatus);
+router.put('/getspecific/:id', orderController.updateOrder);
+router.delete('/getspecific/:id', orderController.deleteOrder);
+router.put('/getspecific/:id/payment', orderController.updatePaymentStatus);
 
 export { router as orderRouter };
